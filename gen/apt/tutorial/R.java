e9880bb1b474068e5bb45f7b29745436aad2969d
Heikki Juva <juva.hj@gmail.com>
2013-04-28 22:20:24 +0300

    Tutorial 3

--- gen/apt/tutorial/R.java
+++ gen/apt/tutorial/R.java
@@ -24,10 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080007;
         public static final int addr=0x7f080001;
+        public static final int delivery=0x7f080005;
         public static final int name=0x7f080000;
-        public static final int save=0x7f080002;
+        public static final int save=0x7f080006;
+        public static final int sit_down=0x7f080004;
+        public static final int take_out=0x7f080003;
+        public static final int types=0x7f080002;
     }
     public static final class layout {
         public static final int activity_lunch_list=0x7f030000;
@@ -39,9 +43,13 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int address=0x7f050004;
         public static final int app_name=0x7f050000;
+        public static final int delivery=0x7f050009;
         public static final int hello_lunchlist=0x7f050002;
         public static final int name=0x7f050003;
         public static final int save=0x7f050005;
+        public static final int sitdown=0x7f050008;
+        public static final int takeout=0x7f050007;
+        public static final int type=0x7f050006;
     }
     public static final class style {
         /** 
